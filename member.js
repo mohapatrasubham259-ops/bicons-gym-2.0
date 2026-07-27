4bd18027c057a668abe665e9ae0cf24c21cfae1f
subham <mohapatrasubham259@gmail.com>
2026-07-27 12:58:07 +0530

    add memeber.js and member.html

---
+++ member.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJ3gjoxKgNTOpLZS-Qg0mrPmp3TVJV7HM",
+  authDomain: "bicon-gym.firebaseapp.com",
+  projectId: "bicon-gym",
+  storageBucket: "bicon-gym.firebasestorage.app",
+  messagingSenderId: "64202444264",
+  appId: "1:64202444264:web:9e3c1c1519431cdbb5a85d"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const table = document.getElementById("membersTable");
+
+async function loadMembers() {
+
+    table.innerHTML = "";
+
+    const querySnapshot = await getDocs(collection(db, "members"));
+
+    querySnapshot.forEach((doc) => {
+
+        const m = doc.data();
+
+        table.innerHTML += `
+        <tr>
+            <td>${m.name}</td>
+            <td>${m.phone}</td>
+            <td>${m.age}</td>
+            <td>${m.plan}</td>
+            <td>₹${m.amount}</td>
+            <td>${m.status}</td>
+        </tr>
+        `;
+    });
+}
+
+loadMembers();
