fcc542df3aec29c7f97d6d9ad0860180a8d6b333
subham <mohapatrasubham259@gmail.com>
2026-07-27 16:24:56 +0530

    Update members.js

--- members.js
+++ members.js
@@ -43,7 +43,7 @@ table.innerHTML += `
 <td>${m.paymentDate}</td>
 <td>${m.expiryDate}</td>
 <td>${m.status}</td>
-<td><button>Edit</button></td>
+<td><button onclick="editMember()">Edit</button></td>
 </tr>
 `;
 });
